ebaaf78b09a0ff9be19aba87021d5f8b3339ceac
Alejandro Galvan <alexgalvanhenandez@gmail.com>
2015-05-21 16:07:22 -0500

    lib phonegap

---
+++ js/testing.js
@@ -0,0 +1,9 @@
+
+function onBodyLoad(){
+	document.addEventListener("deviceready", onDeviceReady, false);
+	
+	}
+function onDeviceReady()
+{
+	$("resultado").html("Esta en marcha jsPhonegap");
+	}
